c5d034df077699cd54278558f79b58ea3e182787
Georgy Boev <boevgo@gmail.com>
2024-06-28 16:40:58 +0300

    Lab2

---
+++ Lab2/mainwindow.cpp
@@ -0,0 +1,43 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QFile>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::keyPressEvent(QKeyEvent *e)
+{
+    if(e->key() == Qt::Key_Up){
+        on_pushButton_3_clicked();
+    }
+}
+
+void MainWindow::on_pushButton_3_clicked()
+{
+    QFile file("in.txt");
+
+    if(!file.exists())
+    {
+        ui->label->setText("File doesn't exists");
+    }
+
+    if(!file.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        ui->label->setText("Can't open the file");
+    }
+
+    int rowcount = file.readLine().toInt();
+
+
+    ui->tableWidget->setRowCount(rowcount);
+}
+
